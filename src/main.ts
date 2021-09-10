b71855fa68e7653673918d6fada02008d4b4cd9d
wjbaker101 <willbaker10198@gmail.com>
2021-09-10 19:25:38 +0100

    In order to parse strings using a specified format, this extension is required

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { createApp } from 'vue';
 import dayjs from 'dayjs';
 
+import customParseFormat from 'dayjs/plugin/customParseFormat';
 import useRelativeTime from 'dayjs/plugin/relativeTime';
 import useUtc from 'dayjs/plugin/utc';
 
 import App from '@/App.vue';
 
+dayjs.extend(customParseFormat);
 dayjs.extend(useRelativeTime);
 dayjs.extend(useUtc);
 
